913155dcd58cd06ec8848ae2d3be6fb1d308db88
Francisco Ferreira <ferreiradeveloper@gmail.com>
2023-10-16 16:59:19 +0200

    variables de entorno configuradas

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BreedsModule } from './breeds/breeds.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+
+    //variables de entrono
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host:'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'db_crud',
+      host: process.env.POSTGRES_HOST, //'localhost',
+      port: parseInt(process.env.POSTGRES_PORT), //5432,
+      username: process.env.POSTGRES_USER, //'root',
+      password: process.env.POSTGRES_PASSWORD, //'root',
+      database: process.env.POSTGRES_DB, //'db_crud',
       //entities: [__dirname + '/**/*-enti{.ts,.js}'],
       autoLoadEntities: true,
       synchronize: true,
